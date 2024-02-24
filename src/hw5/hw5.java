402118b85000b42e98840de7d8e9a67ba4575129
Simeon-Chen <150813140+Simeon-Chen@users.noreply.github.com>
2024-02-24 19:58:20 +0800

    Update hw5.java
    cancel  comment

--- src/hw5/hw5.java
+++ src/hw5/hw5.java
@@ -9,10 +9,10 @@ public class hw5 extends hw5Method{
 //		1.請設計一個方法為starSquare(int width, int height),當使用者鍵盤輸入寬與高時,即會印出對應的*長方形
 		hw5Method method = new hw5Method();
 		Scanner sc = new Scanner(System.in);
-//		System.out.println("請依次輸入寬與高:");
-//		int width = sc.nextInt();
-//		int height = sc.nextInt();
-//		method.starSquare( width, height);
+		System.out.println("請依次輸入寬與高:");
+		int width = sc.nextInt();
+		int height = sc.nextInt();
+		method.starSquare( width, height);
 		
 //		2.請設計一個方法為randAvg(),從10個 0~100(含100)的整數亂數中取平均值並印出這10個亂數與平均值
 		method.randAvg();
